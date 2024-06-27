4f2c7cf0b054593ec7ee978149e2035b5504cabd
Yuliia Popelniak <popelniak96@gmail.com>
2024-06-26 19:28:38 -0500

    UserPrincipal and password encoder are added

--- src/main/java/com/cydeo/BankSimulationAppApplication.java
+++ src/main/java/com/cydeo/BankSimulationAppApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -42,6 +44,11 @@ public class BankSimulationAppApplication {
     @Bean
         public ModelMapper modelMapper(){
             return new ModelMapper();
+        }
+
+        @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
         }
     }
 
